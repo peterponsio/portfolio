ee9bd7dafcb76f0ccaca3441e87fca4c4771d7c4
Pedro Perera <pedrodeveloperera@gmail.com>
2023-02-11 13:05:36 +0100

    fix header, add random elements 3d

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,14 @@
 import { TranslateModule } from '@ngx-translate/core';
 import { Component, OnInit } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass'],
   standalone:true,
-  imports:[TranslateModule,MatIconModule]
+  imports:[TranslateModule,MatIconModule,MatMenuModule]
 })
 export class HeaderComponent implements OnInit {
 
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -2,7 +2,7 @@ import { FooterComponent } from './../../components/footer/footer.component';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from 'src/app/components/header/header.component';
-import { Scene,PerspectiveCamera, WebGLRenderer, TorusGeometry, MeshBasicMaterial, Mesh} from 'three';
+import { Scene,PerspectiveCamera, WebGLRenderer, TorusGeometry, MeshBasicMaterial, Mesh, SphereGeometry, MathUtils} from 'three';
 
 @Component({
   selector: 'app-home',
@@ -29,10 +29,18 @@ export class HomeComponent implements OnInit {
     document.body.appendChild( renderer.domElement );
 
     //Add materials 
+
+    //Torus
     const geometry = new TorusGeometry(10, 3, 16, 100)
     const material = new MeshBasicMaterial({color: "#be123c", wireframe: true})
     const torus = new Mesh( geometry, material)
-    scene.add(torus)
+
+    //Circle
+    const geometryCircle = new SphereGeometry( 5, 32, 16 );
+    const materialCircle = new MeshBasicMaterial( { color: "#be123c", wireframe:true } )
+    const sphere = new Mesh( geometryCircle, materialCircle )
+
+    scene.add(torus,sphere)
 
     //animation loop
     const animate = function(){
@@ -40,9 +48,30 @@ export class HomeComponent implements OnInit {
       torus.rotation.x += 0.01
       torus.rotation.y += 0.01
       torus.rotation.z += 0.01
+      sphere.rotation.x += 0.01
+      sphere.rotation.y += 0.01
+      sphere.rotation.z += 0.01
       renderer.render(scene,camera)
     }
-    animate()
-  }
 
+    const randomElements = function(){
+      const geometry = new SphereGeometry(0.5, 30, 30)
+      const material = new MeshBasicMaterial( { color: "#be123c" } )
+      const star = new Mesh(geometry,material)
+
+     
+      let x = MathUtils.randFloatSpread(100)
+      let y = MathUtils.randFloatSpread(100)
+      let z = MathUtils.randFloatSpread(100)
+      star.position.set(x,y,z)
+      scene.add(star)
+    }
+
+    animate()
+
+    for (let index = 0; index < 100; index++) {
+      randomElements()
+    }
+
+  }
 }
